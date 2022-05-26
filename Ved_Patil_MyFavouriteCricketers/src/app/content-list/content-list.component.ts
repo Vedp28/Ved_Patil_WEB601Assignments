6d989e16c3fe663ac7d4c95d531ac8758d918fb0
VedP2809 <VP83@myscc.ca>
2022-05-26 14:35:51 -0400

    Assignment-3 Commit-1

--- Ved_Patil_MyFavouriteCricketers/src/app/content-list/content-list.component.ts
+++ Ved_Patil_MyFavouriteCricketers/src/app/content-list/content-list.component.ts
@@ -17,7 +17,7 @@ export class ContentListComponent implements OnInit {
       body: 'Virat Kohli is an Indian international cricketer and former captain of the India national cricket team. He plays for Delhi in domestic cricket and Royal Challengers Bangalore in the Indian Premier League as a right-handed batsman',
       author: 'Anushka Sharma',
       imageLink: 'https://www.kreedon.com/wp-content/uploads/2019/01/kohli-7596.jpg.webp',
-      type: 'Batsman',
+      type: 'Batsman, Former Captain',
       hashtags: ['Virat Kohli']
     },
     {
@@ -26,7 +26,7 @@ export class ContentListComponent implements OnInit {
       body: 'Dale Willem Steyn is a South African former professional cricketer who played for the South African Cricket Team. He is often regarded as one of the greatest fast bowlers of all time and the best Test bowler of his generation',
       author: 'Jeanne Kietzmann',
       imageLink: 'https://cdn-wp.thesportsrush.com/2021/08/5752b472-dale-steyn-odi.jpg',
-      type: 'Fast Bowler',
+      type: '',
       hashtags: ['Dale Styne']
     },
     {
@@ -35,7 +35,7 @@ export class ContentListComponent implements OnInit {
       body: 'Abraham Benjamin de Villiers is a former South African international cricketer. AB de Villiers was named as the ICC ODI Player of the Year three times during his 15-year international career and was one of the five Wisden cricketers of the decade at the end of 2019.',
       author: 'Danielle de Villiers',
       imageLink: 'https://images.indianexpress.com/2019/12/ab-de-villiers-1200.jpg',
-      type: 'Wicket-Keeper Batsman',
+      type: 'Wicket-Keeper Batsman, Former Captain',
       hashtags: ['Abraham Benjamin de Villiers']
     },
     {
@@ -44,8 +44,8 @@ export class ContentListComponent implements OnInit {
       body: 'Rohit Gurunath Sharma is an Indian international cricketer who is the current captain of the Indian national team. A right-handed opening batsman and an occasional right-arm off break bowler, he is widely acknowledged as one of the most outstanding batsmen in limited overs cricket.',
       author: ' Ritika Sajdeh',
       imageLink: 'https://www.kreedon.com/wp-content/uploads/2019/03/Rohit-Sharma2.jpg.webp',
-      type: 'Batsman',
-      hashtags: ['Rohit Sharms']
+      type: 'Batsman, Captain',
+      hashtags: ['Rohit Sharma']
     },
     {
       id: 4,
@@ -53,8 +53,26 @@ export class ContentListComponent implements OnInit {
       body: 'James Michael Anderson OBE is an English international cricketer who plays for Lancashire County Cricket Club and the England cricket team. He is also a part time cricket commentator at BBC.',
       author: ' Daniella Lloyd',
       imageLink: 'https://cdn-wp.thesportsrush.com/2021/12/085268f8-josh-inglis-1.jpg',
-      type: 'Fast Bowler',
+      type: '',
       hashtags: ['James Anderson']
+    },
+    {
+      id: 5,
+      title: 'Benjamin Andrew Stokes',
+      body: 'Benjamin Andrew Stokes OBE is an English cricketer who is the current captain of the England Test team and plays for the England One Day International and Twenty20 International teams.',
+      author: ' Clare Ratcliffe',
+      imageLink: 'https://images.news18.com/ibnlive/uploads/2021/12/ben-stokes-confident-about-bowling-in-2nd-test-16394793283x2.jpg?impolicy=website&width=510&height=356',
+      type: 'All-Rounder, Captain',
+      hashtags: ['Ben Stokes']
+    },
+    {
+      id: 6,
+      title: 'Ravindrasinh Anirudhsinh Jadeja',
+      body: 'Ravindrasinh Anirudhsinh Jadeja, commonly known as Ravindra Jadeja, is an Indian international cricketer. He is an all-rounder, who bats left-handed and bowls left-arm orthodox spin. He was the captain of the Chennai Super Kings in the Indian Premier League. He represents Saurashtra in first-class cricket.',
+      author: 'Riva Solanki',
+      imageLink: 'https://images.indianexpress.com/2021/05/ravindra-jadeja-fb.jpg',
+      type: 'All-Rounder',
+      hashtags: ['Ravi Jadeja']
     }
     ]
   }
