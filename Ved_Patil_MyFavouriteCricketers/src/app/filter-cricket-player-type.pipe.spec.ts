f477269d418cef6b1f7fc7ec95303ee7571b3191
VedP2809 <VP83@myscc.ca>
2022-07-08 23:54:33 -0400

    Assignment 4  finally Done

---
+++ Ved_Patil_MyFavouriteCricketers/src/app/filter-cricket-player-type.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterCricketPlayerTypePipe } from './filter-cricket-player-type.pipe';
+
+describe('FilterCricketPlayerTypePipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterCricketPlayerTypePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ Ved_Patil_MyFavouriteCricketers/src/app/filter-cricket-player-type.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Content } from './models/content';
+
+@Pipe({
+  name: 'filterCricketPlayerType'
+})
+export class FilterCricketPlayerTypePipe implements PipeTransform {
+
+  transform(listOfCricketPlayers: Content[], typeToFilterBy?: string): Content[] {
+    return listOfCricketPlayers.filter(filterCricketPlayerType => {
+ 
+        return !filterCricketPlayerType.type?.length 
+         || filterCricketPlayerType.type == "All-Rounder" || filterCricketPlayerType.type =="Batsman" ;
+
+        });
+      }
+    
+    }
