78b371afce9d21239a69a28dce0e50a36a9a83e6
VedP2809 <VP83@myscc.ca>
2022-07-12 18:03:54 -0400

    ChangedContent and added button

--- Ved_Patil_MyFavouriteCricketers/src/app/change-content/change-content.component.ts
+++ Ved_Patil_MyFavouriteCricketers/src/app/change-content/change-content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Content } from '../models/content';
+import { PlayerService } from '../services/player.service';
 
 @Component({
   selector: 'app-change-content',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangeContentComponent implements OnInit {
 
-  constructor() { }
+  contentItem: Content = {
+    title: "",
+    body: "",
+    id: null,
+    author: '',
+    type:'',
+    hashtags: []
+  };
+  tempTags: string = '';
+
+  constructor(private PlayerService: PlayerService) { }
 
   ngOnInit(): void {
   }
 
+  addContentToServer(): void {
+    this.contentItem.hashtags = this.tempTags.split(", ");
+    this.PlayerService.addContentItem(this.contentItem)
+    .subscribe(newContentFromServer =>
+      console.log("Success! New content added", newContentFromServer)
+       );
+   }
+   updateContentOnServer(): void {
+    this.contentItem.hashtags = this.tempTags.split(", ");
+    this.PlayerService.updateContentItem(this.contentItem)
+    .subscribe(() =>
+      console.log("Content updated successfully", this.contentItem)
+    );
+}
 }
