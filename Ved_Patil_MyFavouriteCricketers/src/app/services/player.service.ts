1190daaf52f31fc724667d58014a91cb7d3b69f0
VedP2809 <VP83@myscc.ca>
2022-07-12 17:27:52 -0400

    Update ContentService

--- Ved_Patil_MyFavouriteCricketers/src/app/services/player.service.ts
+++ Ved_Patil_MyFavouriteCricketers/src/app/services/player.service.ts
@@ -2,16 +2,24 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { cricketer, DEFAULTCHESSCONTENT } from '../data/mock-player';
 import { Content } from '../models/content';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
 
-  constructor() { }
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-type':
+        'application/json'
+    })
+  };
+
+  constructor(private http: HttpClient) { }
 
   getContent(): Observable<Content[]> {
-    return of(cricketer);
+    return this.http.get<Content[]>("/api/cricket");
   }
 
   /**
@@ -20,46 +28,33 @@ export class PlayerService {
    * @returns
    */
    getContentItem(id: number): Observable<Content> {
-    return of(cricketer[id]);
+    console.log("Now getting it from the server!");
+    return this.http.get<Content>("/api/cricket/" + id);
   }
 
-  addContentItem(newContentItem: Content): Observable<Content[]> {
+  addContentItem(newContentItem: Content): Observable<Content> {
     // if there aren't some chess champions with the same id number, push the array item
-    if (!cricketer.some(chessPlayer => chessPlayer.id === newContentItem.id)) {
-      cricketer.push(newContentItem);
-    }
-    return of(cricketer);
+    return this.http.post<Content>("/api/cricket", newContentItem, this.httpOptions)
   }
 
   updateContentItem(newContentItem: Content): Observable<Content[]> {
     // find item in chesschampions with same id as newcontentitem's id
     // update the values of that item in the array with the values of the newContentItem
     // return the array after updating
-
-    cricketer.forEach((individualChessChampion, index) => {
-
-      if (individualChessChampion.id === newContentItem.id) // found them
-      {
-        console.log("Trying method 2");
-        cricketer[index] = newContentItem;
-        // return;
-      }
-    });
-
-    return of(cricketer);
+    return this.http.put<any>("api/cricket", newContentItem, this.httpOptions);
   }
 
-  deleteContentItem(id: number): Observable<Content> {
+  // deleteContentItem(id: number): Observable<Content> {
 
-    for (var i = 0; i < cricketer.length; i++) // iterate through each chess champion
-    {
-      if (cricketer[i].id === id) { // found the item
-        // delete it from the array first
-        cricketer.splice(i, 1);
-        console.log("Deleted the item: ", cricketer);
-        return of(cricketer[i]);
-      }
-    }
-    return of(DEFAULTCHESSCONTENT); // need to return something if the content isn't there
-  }
+  //   for (var i = 0; i < cricketer.length; i++) // iterate through each chess champion
+  //   {
+  //     if (cricketer[i].id === id) { // found the item
+  //       // delete it from the array first
+  //       cricketer.splice(i, 1);
+  //       console.log("Deleted the item: ", cricketer);
+  //       return of(cricketer[i]);
+  //     }
+  //   }
+  //   return of(DEFAULTCHESSCONTENT); // need to return something if the content isn't there
+  // }
 }
